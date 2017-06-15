92ffd6b371beb5f0775df40c5cffe4c9e8ca0a7b
dsilva609 <dsilva609@gmail.com>
2017-06-15 19:27:52 -0400

    updated for button press

--- musicplayer.py
+++ musicplayer.py
@@ -1,12 +1,27 @@
-import pygame;
+# import pygame;
 
-def PlayBall():
-	pygame.mixer.init();
-	pygame.mixer.music.load("/home/pi/Downloads/baseball.mp3")
-	pygame.mixer.music.play()
+# def PlayBall():
+	# pygame.mixer.init();
+	# pygame.mixer.music.load("/home/pi/Downloads/baseball.mp3")
+	# pygame.mixer.music.play()
 
-	while pygame.mixer.music.get_busy() == True:
-		continue
+	# while pygame.mixer.music.get_busy() == True:
+		# continue
 
-PlayBall();
-PlayBall();
+# PlayBall();
+# PlayBall();
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+try
+	while True:
+		if (GPIO.input(11) == 1):
+			GPIO.output(7,1)
+		else
+			GPIO.output(7,0)
+except KeyboardInterrupt:
+	GPIO.cleanup()
