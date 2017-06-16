765d1b839c9ac78b43a7ee8bf9c85a343edecb11
dsilva609 <dsilva609@gmail.com>
2017-06-15 20:55:19 -0400

    added back sound for sensor

--- musicplayer.py
+++ musicplayer.py
@@ -2,7 +2,7 @@ import pygame;
 
 def PlayBall():
 	pygame.mixer.init();
-	pygame.mixer.music.load("/home/pi/Downloads/baseball.mp3")
+	pygame.mixer.music.load("/home/pi/Downloads/carelesswhisper.mp3")
 	pygame.mixer.music.play()
 
 	while pygame.mixer.music.get_busy() == True:
@@ -15,15 +15,16 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(7, GPIO.OUT)
 GPIO.output(7,0)
 
 try:
 	while True:
-		if (GPIO.input(11) == 1):
+		if (GPIO.input(11) == 0):
 			GPIO.output(7,1)
+			print "baseball"
 			PlayBall();
 		else:
 			GPIO.output(7,0)
